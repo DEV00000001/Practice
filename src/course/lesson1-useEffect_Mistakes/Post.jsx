391eb7ec5e5dfd31547f722c60fd9e3bc25097ef
DEV00000001 <vuongdachaivang@gmail.com>
2025-09-12 22:22:28 +0700

    #6-clean-up-for- avoiding-memory-leak

--- src/course/lesson1-useEffect_Mistakes/Post.jsx
+++ src/course/lesson1-useEffect_Mistakes/Post.jsx
@@ -3,14 +3,22 @@ import { Link } from "react-router-dom";
 
 function Post() {
   const [posts, setPosts] = React.useState([]);
+  // when component unmounts, fetch the posts is never cancelled, it is running in the background
+  // this may lead to memory leaks 
   useEffect(() => {
+    // if the component is unmounted, we set this flag to true, so we don't update the state, and rerender the component
+    let isCancelled = false;
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((data) => {
+        if (isCancelled) return;
         alert("posts are ready !, updating the state");
         setPosts(data);
         console.log(data);
       });
+    return () => {
+      isCancelled = true;
+    };
   }, []);
   return (
     <>
