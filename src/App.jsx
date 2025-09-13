0e8739212909ddb9dfd82d0b8427d3b28cd322a5
DEV00000001 <vuongdachaivang@gmail.com>
2025-09-13 15:14:14 +0700

    #1-normal-object-date

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Home from "./course/lesson1-useEffect_Mistakes/Home";
 import Lesson1_1 from "./course/lesson1-useEffect_Mistakes/index2";
 import Lesson1_2 from "./course/lesson1-useEffect_Mistakes/index3";
 import Post from "./course/lesson1-useEffect_Mistakes/Post";
+import Lesson2_0 from "./course/lesson2-objectDate";
 function App() {
   return (
     <>
       {/* <Lesson1_0/> */}
       {/* <Lesson1_1/> */}
       {/* <Lesson1_2/> */}
+      <Lesson2_0/>
 
       <Routes>
         <Route path="/" element={<Home />} />
