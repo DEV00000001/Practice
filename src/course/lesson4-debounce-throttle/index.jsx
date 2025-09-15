bcf8311af720458ac877c6cb0866f4dd76c321da
DEV00000001 <vuongdachaivang@gmail.com>
2025-09-15 21:19:21 +0700

    this-is-error-if-u-dont-understand-debounce-in-react

--- src/course/lesson4-debounce-throttle/index.jsx
+++ src/course/lesson4-debounce-throttle/index.jsx
@@ -13,6 +13,19 @@ function Lesson4_0() {
       .then(data => setResults(data.users))
       .catch(err => console.error(err));
   };
+  // debounce: it means to limit the rate at which a function can fire.
+  // it will wait for the user to stop typing for a certain amount of time before making the API call
+  
+  const debounce = (callback, delay) => {
+    let timeout = null;
+
+    return (...args) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        callback(...args);
+      }, delay);
+    }
+  };
 
   return (
     <div style={{display: "flex", alignItems: "center", flexDirection: "column", gap: "20px", marginTop: "50px"}}>
@@ -21,7 +34,9 @@ function Lesson4_0() {
         <input
           type="text"
           value={searchText}
-          onChange={handleInputChange}
+          // this is error , because handleInputChange only runs when user stop typing for 1 second
+          // at this time, text in the input box : "" , so API call will be made with empty string
+          onChange={debounce(handleInputChange, 1000)}
         />  
       </div>
       {results.length > 0 && (
